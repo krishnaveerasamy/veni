39f43ba1255935e49ef122e6bd0ea801a728ab7a
krishnaveerasamy <36039163+krishnaveerasamy@users.noreply.github.com>
2018-02-19 16:25:54 +0530

    stringchange19.c
    program

---
+++ stringchange19.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i;
+clrscr();
+char str[20];
+printf("enter the string\n");
+scanf("%d",%s);
+for(i=0;i<=strlen(str);i++)
+{
+if(str[i]>=97&&str[i]<=122);
+{
+str[i]=str[i]-32;
+}
+}
+printf("the string is\n");
+scanf("%d,&str);
+}
