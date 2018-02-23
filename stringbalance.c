3c89b93007b3a6d4bb666234fa14d5e2884b15bd
krishnaveerasamy <36039163+krishnaveerasamy@users.noreply.github.com>
2018-02-23 16:55:21 +0530

    stringbalance.c
    program

---
+++ stringbalance.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i=0,j,num;
+clrscr();
+printf("\n enter the string");
+for(i=0;i<num;i++)
+{
+printf("\nits dtring");
+}
+else
+{
+printf("its not string\n");
+}
+}
+}
