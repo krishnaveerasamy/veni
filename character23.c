955e2237b41ff01605eb5e5e881204b501aea1b4
krishnaveerasamy <36039163+krishnaveerasamy@users.noreply.github.com>
2018-02-23 15:52:40 +0530

    character23.c
    program

---
+++ character23.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,count;
+clrscr();
+printf("\n enter the characters\n");
+scanf("%d",&n);
+if(count<999)
+{
+printf("\n yes");
+else
+{
+printf("\n no");
+}
+}
+}
