3a92bbe6a89f3c15304ce7244671ec4bf28b958b
krishnaveerasamy <36039163+krishnaveerasamy@users.noreply.github.com>
2018-02-26 16:13:13 +0530

    ARRAY26.C
    program

---
+++ ARRAY26.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+int n,k,i,count=0,array[100];
+printf("enter array size:");
+scanf("%d",&n);
+printf("enter elements to be searched\n");
+scanf("%d",&k);
+printf("enter the elements\n");
+for(i=0;i<n;i++)
+{
+scanf("%^d",a[i]);
+}
+for(i=0;i<n;i++)
+{
+if(a[i]=k)
+{
+printf("\n yes");
+count=1;
+}
+}
+if(count==0)
+{
+print("\n no");
+}return 0;
+}
