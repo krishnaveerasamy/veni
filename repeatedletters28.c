6c0edb918f782812df4a7646beebde3951f37e9f
krishnaveerasamy <36039163+krishnaveerasamy@users.noreply.github.com>
2018-02-28 02:16:38 -0800

    repeatletter28.c
    program

---
+++ repeatedletters28.c
@@ -0,0 +1,11 @@
+#inlcude<stdio.h>
+#include<string.h>
+main()
+{
+char *ptr="vishaal";
+char name[50]="yadav";
+printf("the length of string:\n");
+printf("vishaal is %d \n",strlen("vishaal"));
+printf("yadav is %d \n ",strlen("yadav"));
+printf("pradep is %d\n",strlen(name));
+}
