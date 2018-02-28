e2e665d71c02a3aa46fb363ff8f697a98e165b5a
krishnaveerasamy <36039163+krishnaveerasamy@users.noreply.github.com>
2018-02-28 03:10:03 -0800

    powerofnum28.c
    program

---
+++ powerofnum28.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,num=0,i=0;
+printf("enter the numbers");
+scanf("%d",&n);
+for(i=2;i<n,i++)
+num=num+1;
+break;
+}
+if(num==1)
+printf("it is a power of given number");
+else
+printf("it is not a power of given number");
+getch();
+}
