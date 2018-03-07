d16122d372a4ef4adbb2b95e2fb570c1d0a0a891
krishnaveerasamy <36039163+krishnaveerasamy@users.noreply.github.com>
2018-03-07 15:49:25 +0530

    shorted array7.c
    program

---
+++ shorted array7.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+vid main()
+{
+int n,sum,num;
+printf("enter the array elements\n");
+for(i=o;i<=100;i++)
+{
+printf("sorted array is\n");
+scanf("%d",&num);
+else
+{
+printf("give the valid array elements\n");
+scanf("%d",&sum);
+getch();
+}
+}
