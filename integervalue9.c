4af9621b5659b2c5c2efcaefa20fba5cda37955d
krishnaveerasamy <36039163+krishnaveerasamy@users.noreply.github.com>
2018-03-09 16:24:28 +0530

    integervalue9.c
    program

--- integervalue9.c
+++ integervalue9.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include<conio.h>
 int main()
 {
     int number;
+    clrscr();
     printf("Enter an integer: ");  
     scanf("%d", &number);  
     printf("You entered: %d", number);
