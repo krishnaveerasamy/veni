82d39a3feaa490b50bca494d6d80b0a70e8338f2
krishnaveerasamy <36039163+krishnaveerasamy@users.noreply.github.com>
2018-03-09 15:40:57 +0530

    reverseword9.c
    program

---
+++ reverseword9.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num,count=0;
+printf("enter the word:");
+scanf("%d",&num);
+while(num!=0)
+{
+digit=num%10;
+num=num/10;
+reverse reverse*10+digit;
+}
+printf("reverse of wors"%d",&reverse);
+}
