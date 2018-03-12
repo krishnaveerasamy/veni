d4e1b4d353287993a7babe0b1b8340e2617288f5
krishnaveerasamy <36039163+krishnaveerasamy@users.noreply.github.com>
2018-03-12 15:47:59 +0530

    divisable12.c
    program

---
+++ divisable12.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num,digit;
+clrscr();
+for(i=0;i<=num;i++)
+{
+printf("\n enter two umbers");
+scanf("%d",&num);
+while(digit==num)
+{
+printf("yes\n");
+}
+}
