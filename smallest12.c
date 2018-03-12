c3b9bf3bdca93be421805252f745654b7ea07fea
krishnaveerasamy <36039163+krishnaveerasamy@users.noreply.github.com>
2018-03-12 16:01:51 +0530

    smallest12.c
    program

---
+++ smallest12.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int digit,n;
+clrscr();
+printf("enter the numbers\n\n");
+if(n>0)
+{
+printf("the smallest number is....");
+scanf("%d",&num);
+}else
+{
+printf("its not not smallest number....\n\n");
+}
+}
