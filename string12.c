62f737e1c9b8cf553a4fb0f99c5852a41463feaa
krishnaveerasamy <36039163+krishnaveerasamy@users.noreply.github.com>
2018-03-12 16:24:49 +0530

    string12.c
    program

---
+++ string12.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char string[50];
+int i,length=0;
+clrscr();
+printf("\n enter the string");
+gets(strings);
+for(i=0;string i!='0';i++)
+{
+length++;
+}
+printf("legnth of the string in this number\n\n");
+printf("the string lengths are\n");
+}
